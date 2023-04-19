dd9112df7ab3035235fcca36e9f36fbe6d61cfde
tcsresearch <tcsresearch@users.noreply.github.com>
2023-04-19 02:29:32 -0500

    Update and rename Upgrade-Espo.sh to tarmonster.sh

---
+++ tarmonster.sh
@@ -0,0 +1,20 @@
+## tarmonster.sh
+# This is the main script for TarMonster.
+
+## First, we will import our variables ##
+TODO: Add Sanity Check.
+source ~/bin/TarMonster.vars
+
+### Next, we import our functions ###
+TODO: Add Sanity Check.
+source ~/bin/TarMonster.bfunc
+
+##  Call Functions ###
+ DisplayVars
+ SanityChecker
+ DoBackup
+ CompressBackup
+ MoveBackup
+ # For EspoCRM Only
+ # DoUpgrade
+
