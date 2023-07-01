a4b54396b6c506590f3050884762ba6ca3905151
tcsresearch <tcsresearch@users.noreply.github.com>
2023-07-01 00:36:57 -0500

    Remove references to ESPOCRM.
    Test code which may already be superceded.

---
+++ _TarMonster-20230701-1/TarMonster.sh
@@ -0,0 +1,20 @@
+## TarMonster.sh
+# This script will backup and upgrade the selcted app.
+
+## First, we will import our variables ##
+source ~/bin/TarMonster.vars
+
+### Next, we import our functions ###
+
+source ~/bin/TarMonster.bfunc
+
+##  Call Functions ###
+ DisplayBanner
+ DisplayVars | column -t -s $'\t' # Pipes to colum for properly aligned tabbed output
+ Pause This script will backup the selected application and perform any available upgrades.  Press Any Key To Continue...
+ SanityChecker
+ DoBackup
+ CompressBackup
+ MoveBackup
+ DoUpgrade
+
