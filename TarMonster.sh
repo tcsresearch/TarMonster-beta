d2f0308de25eab7ecce94fe6cfc6286a993199cd
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2025-08-31 00:58:50 -0500

    Update TarMonster.sh

--- TarMonster.sh
+++ TarMonster.sh
@@ -1,21 +1,34 @@
-## TarMonster.sh
-# This script will backup and upgrade the selcted app.
+## tarmonster3.sh
+# This is the Cecho Enabled / File & Folder Restructured main script for TarMonster.
+# Currently set as DisplayVarsOnly.
 
-## First, we will import our variables ##
-# source ~/bin/TarMonster.vars
-source bin/TarMonster.vars
+## First, we will import our Master Config File ##
+# TODO: Add Sanity Check.
+source ./conf/TarMonster.conf
+
+## Second, we will import our additional config files.
+# TODO: Add Sanity Check.
+source ./conf/Application.conf # Will eventually become plugins under conf.d or plugins.d
+source ./conf/Compression.conf
+source ./conf/FilesFolders.conf
 
 ### Next, we import our functions ###
+# TODO: Add Sanity Check.
+source ./functions/TarMonster.bfunc
 
-# source ~/bin/TarMonster.bfunc
-source bin/TarMonster.bfunc
+### Enable ColorEcho Library ###
+# TODO: Add Sanity Check.
+source ./libraries/Colors.shlib
+
 ##  Call Functions ###
- DisplayBanner
- DisplayVars | column -t -s $'\t' # Pipes to colum for properly aligned tabbed output
- Pause This script will backup the selected application and perform any available upgrades.  Press Any Key To Continue...
+ DisplayBanner2
+ DisplayVars2
  SanityChecker
- DoBackup
- CompressBackup
- MoveBackup
- DoUpgrade
-
+ 
+ # DoBackup
+ # CompressBackup
+ # MoveBackup
+ 
+ 
+ # For EspoCRM Only
+ # DoUpgrade
