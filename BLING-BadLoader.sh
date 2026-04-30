d5bff6dc02bc142acdaa9922d5d032a430df5b6b
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-04-30 00:39:12 -0500

    Fix errors in BLING-BadLoader.sh
    Fixed critical errors in folder checks and echo statements.

--- BLING-BadLoader.sh
+++ BLING-BadLoader.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: Fixed a critical error...will this work now?
+
 BLING_Libs_Folder="./BLING"
 
 echo "BLING Loader"
@@ -8,15 +10,15 @@ echo " "
 
 function BadLoader() {
 if [ -d "$BLING_Libs_Folder" ]; then
-	:echo "Library Folder Exists [OK]"
-	cd $BLING_Libs_Folder
+	echo "Library Folder Exists [OK]"
+	cd "$BLING_Libs_Folder"
        # cecho blue "Processing BLING Library File: "
 	echo "Processing BLING Library File: "
   	# Folder exists, start the for loop
   	for BLING_LibraryFile in "$BLING_Libs_Folder"/*.bfunc; do
     		# cecho blue "Processing BLING Library: "
     		echo -e "$BLING_LibraryFile"
-    		source $BLING_LibraryFile
+    		source "$BLING_LibraryFile"
 	cd -
 	done
 else
@@ -28,18 +30,18 @@ fi
 function Loader() {
 	if [ -d "BLING_Libs_Folder" ]; then
 		echo "Library Folder Exists: [OK]"
-		cd $BLING_Libs_Folder
+		cd "$BLING_Libs_Folder"
 		# cecho blue "Processing BLING Library File: "
 		echo "Processing BLING Library File: "
 		# Folder exists, start the for loop
-		cd $BLING_Library_Folder
+		cd "$BLING_Library_Folder"
 		sh BLING_TempLoader.sh
 		cd -
 		echo "BLING Libraries Loaded Successfully."
 		cd -
 	else
 		# cecho red "ERROR: Folder does not exists: $BLING_Libs_Folder"
-		echo "ERROR: Folder does not exists: $BLING_Libs_Folder"
+		echo "ERROR: Folder does not exist: $BLING_Libs_Folder"
 	fi
 }
 
