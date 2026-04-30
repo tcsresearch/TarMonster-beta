8ff4927f44e53d8fd059a838b4fc77e65279f6e9
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-04-30 01:36:25 -0500

    Update source paths to use quotes in TarMonster2.sh

--- TarMonster2.sh
+++ TarMonster2.sh
@@ -18,11 +18,11 @@ echo "Batch Loading BLING Libraries..."
 # source `cat ./BLING/BLING-Functions.list`
 
 function BLING_Loader1() {
-	source ./BLING/Cecho.bfunc
-	source ./BLING/CommandExists.bfunc
-	source ./BLING/Today.bfunc
-	source ./BLING/die.bfunc
-	source ./BLING/duls.bfunc
+	source "./BLING/Cecho.bfunc"
+	source "./BLING/CommandExists.bfunc"
+	source "./BLING/Today.bfunc"
+	source "./BLING/die.bfunc"
+	source "./BLING/duls.bfunc"
 }
 function CechoTest() {
 	## Cecho Test ##
@@ -32,12 +32,12 @@ function CechoTest() {
 
 function LoadFuncVars() {
 	## Source Variables ##
-	source bin/TarMonster.vars
+	source "bin/TarMonster.vars"
 
 	### Next, we import our functions ###
 
 	# source ~/bin/TarMonster.bfunc
-	source bin/TarMonster.bfunc
+	source "bin/TarMonster.bfunc"
 }
 
 
