183c0d16348ad17540300e1579c6fe30c9150cf8
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-04-30 01:06:03 -0500

    Replace 'cd -' with 'cd ...' for shellcheck compliance
    Updated directory change command for better compatibility.

--- GetCurrentAppVersion2.sh
+++ GetCurrentAppVersion2.sh
@@ -5,5 +5,5 @@ php -r "echo 'Current EspoCRM Vesrion Is:' "  && php command.php version
 echo " "
 php command.php version > VERSION
 echo "Saved Current EspoCRM Version to crm.tcshosting.net/VERSION"
-cd -
+cd ... || return # use instead of 'cd -' (per shellcheck)
 
