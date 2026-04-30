5df9ed24db0ffd81c9f98f6604f6c60da046b33c
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-04-30 01:26:03 -0500

    Fix cd command and improve source command syntax

--- _BLING_TempLoader.sh
+++ _BLING_TempLoader.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cd BLING
+cd BLING || return 1
 pwd
 #  source Cecho.bfunc CommandExists.bfunc Pause.bfunc Today.bfunc die.bfunc duls.bfunc
-source `cat BLING-Functions.list`
+source $(cat BLING-Functions.list)
 cd ... || return # use instead of 'cd -' (per shellcheck)
