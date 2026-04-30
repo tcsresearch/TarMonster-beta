499a629f3a46e7c405192c783108e55f6e08d2aa
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-04-30 01:45:31 -0500

    Fix source command syntax in _BLING_TempLoader.sh

--- _BLING_TempLoader.sh
+++ _BLING_TempLoader.sh
@@ -3,5 +3,5 @@
 cd BLING || return 1
 pwd
 #  source Cecho.bfunc CommandExists.bfunc Pause.bfunc Today.bfunc die.bfunc duls.bfunc
-source $(cat BLING-Functions.list)
+source "$(cat BLING-Functions.list)"
 cd ... || return # use instead of 'cd -' (per shellcheck)
