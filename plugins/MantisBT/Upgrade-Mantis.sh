4e467ee54f0ad04d5cc21474e36912298a7d3356
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-20 03:37:50 -0500

    Add shellcheck directive to Upgrade-Mantis.sh

--- plugins/MantisBT/Upgrade-Mantis.sh
+++ plugins/MantisBT/Upgrade-Mantis.sh
@@ -4,10 +4,11 @@
 # This script will backup and upgrade MantisBT
 
 ## First, we will import our variables ##
+# shellcheck source=/dev/null
 source ~/bin/MantisBT.vars
 
 ### Next, we import our functions ###
-
+# shellcheck source=/dev/null
 source ~/bin/MantisBT.bfunc
 
 ##  Call Functions ###
