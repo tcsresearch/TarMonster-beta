9ffeadbbd6ac191f6dfaba14cde261ab9dd09a43
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-20 03:40:29 -0500

    Add shellcheck directive to _BLING_TempLoader.sh
    Add shellcheck directive for sourced file.

--- _BLING_TempLoader.sh
+++ _BLING_TempLoader.sh
@@ -3,5 +3,6 @@
 cd BLING || return 1
 pwd
 #  source Cecho.bfunc CommandExists.bfunc Pause.bfunc Today.bfunc die.bfunc duls.bfunc
+# shellcheck source=/dev/null
 source "$(cat BLING-Functions.list)"
 cd ... || return # use instead of 'cd -' (per shellcheck)
