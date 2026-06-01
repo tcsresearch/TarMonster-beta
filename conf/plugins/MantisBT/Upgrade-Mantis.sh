cbb814d4a776df3f1970f5127694df220e54ce13
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-31 21:31:02 -0500

    Add ShellCheck directives to Upgrade-Mantis.sh
    Added ShellCheck directives to suppress warnings for unused variables and sourced files.

--- conf/plugins/MantisBT/Upgrade-Mantis.sh
+++ conf/plugins/MantisBT/Upgrade-Mantis.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# ShellCheck: Disable SC1090
+# shellcheck source=/dev/null
+# ShellCheck: Disable SC2034
+# shellcheck disable=SC2034  # Unused variables left for readability
 
 ## Upgrade-Mantis.sh
 # This script will backup and upgrade MantisBT
