6c789acbebac15d573e54edc6074026bce8f3737
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-31 21:36:09 -0500

    Add ShellCheck directives for unused variables
    Added ShellCheck directives to ignore specific warnings.

--- TarMonster3.sh
+++ TarMonster3.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# ShellCheck: Disable SC1090
+# shellcheck source=/dev/null
+# ShellCheck: Disable SC2034
+# shellcheck disable=SC2034  # Unused variables left for readability
+
 
 # echo "Enabling Cecho..."
 # FIXME: Add Cecho.bfunc
